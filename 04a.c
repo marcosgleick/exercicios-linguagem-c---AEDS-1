158e7ca3ddaf9084f180161f9f0a18da03809904
marcosgleick <111712207+marcosgleick@users.noreply.github.com>
2022-09-01 13:51:54 -0300

    Add files via upload

---
+++ 04a.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <math.h>
+
+//04- a)Leia um número e impra o seu sucessor.//
+
+    int main()
+
+{
+        int n1,sucessor;
+
+        printf("Insira um numero:");
+     scanf("%d", &n1);
+     sucessor=n1+1;
+     printf("O sucesso de %d é %d.",n1,sucessor);
+
+
+
+
+
+
+
+
+    return 0;
+}
